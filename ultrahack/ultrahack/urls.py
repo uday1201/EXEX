864270fa41857bdf2e02a87d6e9bc7bd215be025
Jatin Katyal <jatin.katyal13@gmail.com>
2018-05-20 00:50:55 +0530

    fghjkl

--- ultrahack/ultrahack/urls.py
+++ ultrahack/ultrahack/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
 
 from django.contrib import admin
 from django.conf.urls import url, include
+from django.urls import path
 from rest_framework import routers
 from exex import views
 
